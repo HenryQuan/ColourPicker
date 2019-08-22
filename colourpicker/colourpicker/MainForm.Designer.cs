1fdb26c62fd77e52afec9409d1f7874d39e8efd8
Yiheng (Surface) <development.henryquan@gmail.com>
2019-08-22 13:03:20 +1000

    working but there are offsets

--- colourpicker/colourpicker/MainForm.Designer.cs
+++ colourpicker/colourpicker/MainForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace ColourPicker
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         /// Required designer variable.
@@ -28,21 +28,45 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.colourPanel = new System.Windows.Forms.Panel();
+            this.colorLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // Form1
+            // colourPanel
+            // 
+            this.colourPanel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.colourPanel.Location = new System.Drawing.Point(12, 12);
+            this.colourPanel.Name = "colourPanel";
+            this.colourPanel.Size = new System.Drawing.Size(212, 212);
+            this.colourPanel.TabIndex = 0;
+            // 
+            // colorLabel
+            // 
+            this.colorLabel.Location = new System.Drawing.Point(243, 12);
+            this.colorLabel.Name = "colorLabel";
+            this.colorLabel.Size = new System.Drawing.Size(321, 23);
+            this.colorLabel.TabIndex = 1;
+            this.colorLabel.Text = "...";
+            // 
+            // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(576, 236);
+            this.Controls.Add(this.colorLabel);
+            this.Controls.Add(this.colourPanel);
+            this.MaximizeBox = false;
             this.MinimumSize = new System.Drawing.Size(600, 300);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.Name = "MainForm";
+            this.Text = "Colour Picker";
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel colourPanel;
+        private System.Windows.Forms.Label colorLabel;
     }
 }
 
--- colourpicker/colourpicker/MainForm.cs
+++ colourpicker/colourpicker/MainForm.cs
@@ -4,17 +4,71 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace ColourPicker
 {
-    public partial class Form1 : Form
+    public partial class MainForm : Form
     {
-        public Form1()
+        Point lastLocaion;
+        Color currColour;
+
+        public MainForm()
         {
             InitializeComponent();
+            lastLocaion = Cursor.Position;
+
+            Timer t = new Timer();
+            t.Interval = 10;
+            t.Enabled = true;
+            t.Tick += T_Tick;
+        }
+
+        private void T_Tick(object sender, EventArgs e)
+        {
+            Point curr = Cursor.Position;
+            Console.WriteLine(curr);
+            // Same location don't update anything
+            if (lastLocaion.X == curr.X && lastLocaion.Y == curr.Y) return;
+
+            // Update panel colour
+            updateColour(GetColorAt(curr));
+
+        }
+
+        private void updateColour(Color c)
+        {
+            currColour = c;
+            colourPanel.BackColor = c;
+
+            // Get RGB and HEX string
+            colorLabel.Text = string.Format("RGB({0}, {1}, {2})", c.R, c.G, c.B);
+        }
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern IntPtr GetDesktopWindow();
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern IntPtr GetWindowDC(IntPtr window);
+        [DllImport("gdi32.dll", SetLastError = true)]
+        public static extern uint GetPixel(IntPtr dc, int x, int y);
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern int ReleaseDC(IntPtr window, IntPtr dc);
+
+        /// <summary>
+        /// Get pixel colour at x, y, from https://stackoverflow.com/questions/1483928/how-to-read-the-color-of-a-screen-pixel#1483963
+        /// </summary>
+        /// <param name="p">Cursor position</param>
+        /// <returns>Color</returns>
+        public static Color GetColorAt(Point p)
+        {
+            IntPtr desk = GetDesktopWindow();
+            IntPtr dc = GetWindowDC(desk);
+            int a = (int)GetPixel(dc, p.X, p.Y);
+            ReleaseDC(desk, dc);
+            return Color.FromArgb(255, (a >> 0) & 0xff, (a >> 8) & 0xff, (a >> 16) & 0xff);
         }
     }
 }
